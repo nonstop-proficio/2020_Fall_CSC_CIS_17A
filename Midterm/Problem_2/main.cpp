16ed109777d4b057adbf4aa63eb22003dd4c7e24
Antero <anteroavalos2508@gmail.com>
2020-10-19 23:54:01 -0700

    Problem 2, incomplete

--- Midterm/Problem_2/main.cpp
+++ Midterm/Problem_2/main.cpp
@@ -33,14 +33,51 @@ Printout like a company Paycheck with format:
 //System Libraries
 #include <cstdlib>
 #include <iostream>
+#include <string>
 using namespace std;
 
 //User Libraries
 #include "Employee.h"
 
+//Function Prototypes
+int numEmpl();          //Gets number of employees for payroll 
+Employee * fillAry(int);   //Fill struct array with payroll data
+
+//--------------------------MAIN starts here--------------------
 int main(int argc, char** argv) {
-    
+    int size = numEmpl();
+
+    Employee * payroll;
+
+    payroll = fillAry(size);
+
 
     return 0;
 }
+//--------------------------MAIN ends here---------------------
+int numEmpl(){
+    int nWrkrs;
+    
+    cout << "Enter the number of employees you need checks for: ";
+    cin >> nWrkrs;
 
+    return nWrkrs;
+}
+
+
+Employee * fillAry(int arySize){
+    Employee * payroll = new Employee[arySize];
+
+    for (int i = 0; i < arySize; i++)
+    {
+        cin.ignore();
+        cout << "Enter employee's name: ";
+        getline(cin, payroll[i].name);
+        cout << "Hours worked: ";
+        cin >> payroll[i].workHrs;
+        cout << "Employee's pay rate: ";
+        cin >> payroll[i].payRate;
+    }
+    
+    return payroll;
+}
