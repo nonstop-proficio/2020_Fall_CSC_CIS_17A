98b08759902fce2dd602be4dc5996437be64776f
Antero <anteroavalos2508@gmail.com>
2020-10-19 21:27:47 -0700

    Problem 2, incomplete

---
+++ Midterm/Problem_2/main.cpp
@@ -0,0 +1,46 @@
+/*
+2)  Develop an application using an array of structures that will 
+determine the gross pay for any number of employees input. The company pays 
+"straight-time" for the first 40 hours worked, double time for 
+all hours worked in excess of 40 hours but less than 50 hours, 
+and triple time for any hours worked over 50 hours.
+
+The program should be able to loop and input the employee's name, 
+hours worked, and the rate of pay.  Once this has been done then output
+this information including the gross pay in the form of a paycheck.
+The process starts all over 
+again until you input an invalid rate of pay or hours worked.  This
+means a negative rate of pay or negative number of hours worked is
+not acceptable.
+
+Print it out like an actual paycheck with English
+language representing the check amount.
+Printout like a company Paycheck with format:
+     Company
+     Address
+     Name:	Amount: numerical
+     Amount:    English of numerical value
+     Signature Line:
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Antero
+ *
+ * Created on October 19, 2020
+ */
+
+//System Libraries
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+//User Libraries
+#include "Employee.h"
+
+int main(int argc, char** argv) {
+    
+
+    return 0;
+}
+
