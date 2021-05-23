37f152e72e3ca23b72c2feeb318e47337f31022a
Antero <anteroavalos2508@gmail.com>
2021-05-23 13:56:38 -0700

    Homework 5

--- Hmwk/Homework5/14.2/nbproject/Makefile-variables.mk
+++ Hmwk/Homework5/14.2/nbproject/Makefile-variables.mk
@@ -9,19 +9,19 @@ CND_DISTDIR=dist
 # Debug configuration
 CND_PLATFORM_Debug=Cygwin-Windows
 CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
-CND_ARTIFACT_NAME_Debug=14.23
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/14.23
+CND_ARTIFACT_NAME_Debug=14.2
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/14.2
 CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
-CND_PACKAGE_NAME_Debug=14.23.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/14.23.tar
+CND_PACKAGE_NAME_Debug=14.2.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/14.2.tar
 # Release configuration
 CND_PLATFORM_Release=Cygwin-Windows
 CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
-CND_ARTIFACT_NAME_Release=14.23
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/14.23
+CND_ARTIFACT_NAME_Release=14.2
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/14.2
 CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
-CND_PACKAGE_NAME_Release=14.23.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/14.23.tar
+CND_PACKAGE_NAME_Release=14.2.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/14.2.tar
 #
 # include compiler specific variables
 #
--- Hmwk/Homework5/14.2/nbproject/Package-Release.bash
+++ Hmwk/Homework5/14.2/nbproject/Package-Release.bash
@@ -7,15 +7,15 @@
 # Macros
 TOP=`pwd`
 CND_PLATFORM=Cygwin-Windows
-CND_CONF=Debug
+CND_CONF=Release
 CND_DISTDIR=dist
 CND_BUILDDIR=build
 CND_DLIB_EXT=dll
 NBTMPDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/14.23
-OUTPUT_BASENAME=14.23
-PACKAGE_TOP_DIR=14.23/
+OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/14.2
+OUTPUT_BASENAME=14.2
+PACKAGE_TOP_DIR=14.2/
 
 # Functions
 function checkReturnCode
@@ -60,15 +60,15 @@ mkdir -p ${NBTMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory "${NBTMPDIR}/14.23/bin"
+makeDirectory "${NBTMPDIR}/14.2/bin"
 copyFileToTmpDir "${OUTPUT_PATH}.exe" "${NBTMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}.exe" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/14.23.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/14.2.tar
 cd ${NBTMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/14.23.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/14.2.tar *
 checkReturnCode
 
 # Cleanup
